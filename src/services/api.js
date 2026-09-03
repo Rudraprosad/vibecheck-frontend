e9d837b2aece95f30a6252f570ca93077c4cad92
Rudraprosad <166218847+Rudraprosad@users.noreply.github.com>
2026-09-03 16:51:50 +0530

    axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://vibecheck-backend-yg08.onrender.com/api',
 });
 
 // Intercept requests to add the Authorization header
